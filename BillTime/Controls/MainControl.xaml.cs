fa978d9da33ce8ef8fcdd32643daa8fd0f4cfb94
Raúl Grados Carreño <gnr2092@gmail.com>
2021-02-17 11:56:55 -0500

    Main Form Setup
    Designed the menu and the main user control that
    compose the main form.

--- BillTime/Controls/MainControl.xaml.cs
+++ BillTime/Controls/MainControl.xaml.cs
@@ -1,5 +1,4 @@
-﻿using BillTime.Controls;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,17 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BillTime
+namespace BillTime.Controls
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainControl.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainControl : UserControl
     {
-        public MainWindow()
+        public MainControl()
         {
             InitializeComponent();
-            content.Content = new MainControl();
         }
     }
 }
