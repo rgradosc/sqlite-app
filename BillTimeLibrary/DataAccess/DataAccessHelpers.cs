2f1e40a59625f181dc02cbd6e385a4e07c2830ad
Raúl Grados Carreño <gnr2092@gmail.com>
2021-07-05 16:57:29 -0500

    Setting up the data access

---
+++ BillTimeLibrary/DataAccess/DataAccessHelpers.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+
+namespace BillTimeLibrary.DataAccess
+{
+    public static class DataAccessHelpers
+    {
+        internal static string LoadConnectionString(string connectionName = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+        }
+    }
+}
---
+++ BillTimeLibrary/DataAccess/SqliteDataAccess.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace BillTimeLibrary.DataAccess
+{
+    public static class SqliteDataAccess
+    {
+        public static List<T> LoadData<T>(string sqlStatement, Dictionary<string, object> parameters, string connectionName = "Default")
+        {
+            DynamicParameters p = parameters.ToDynamicParameters();
+
+            using (IDbConnection connection = new SQLiteConnection(DataAccessHelpers.LoadConnectionString(connectionName)))
+            {
+                var rows = connection.Query<T>(sqlStatement, p);
+
+                return rows.ToList();
+            }
+        }
+
+        public static void SaveData(string sqlStatement, Dictionary<string, object> parameters, string connectionName = "Default")
+        {
+            DynamicParameters p = parameters.ToDynamicParameters();
+
+            using (IDbConnection connection = new SQLiteConnection(DataAccessHelpers.LoadConnectionString(connectionName)))
+            {
+                connection.Execute(sqlStatement, p);
+            }
+        }
+
+        private static DynamicParameters ToDynamicParameters(this Dictionary<string, object> parameters)
+        {
+            DynamicParameters p = new DynamicParameters();
+            parameters.ToList().ForEach(x => p.Add(x.Key, x.Value));
+            return p;
+        }
+    }
+}
